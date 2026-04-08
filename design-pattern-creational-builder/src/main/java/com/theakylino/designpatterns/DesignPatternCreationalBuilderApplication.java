f7272fcf176d08ccfdb67c690e57a4a703fd76fc
Atilio Olazo <40051292+TheAkylino@users.noreply.github.com>
2026-04-07 22:45:01 -0500

    creation of the design pattern creational builder project

---
+++ design-pattern-creational-builder/src/main/java/com/theakylino/designpatterns/DesignPatternCreationalBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.theakylino.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternCreationalBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternCreationalBuilderApplication.class, args);
+	}
+
+}
